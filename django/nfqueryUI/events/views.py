63ffe2cd19cb2f2f3b494a4a53b99139d8d78a7c
Serhat Rıfat Demircan <demircan.serhat@gmail.com>
2013-05-05 16:42:55 +0300

    event/index view sends all log info to template

--- django/nfqueryUI/events/views.py
+++ django/nfqueryUI/events/views.py
@@ -2,8 +2,12 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from nfqueryUI.lib.rpc.rpcutils import get_log
+import sys
 
 @login_required
 def index(request):
     data = {'tabName': "events", 'pageTitle': "Events"}
+    log = get_log()
+    data['log'] = log
     return render_to_response('base/layout.html', data, context_instance=RequestContext(request))
